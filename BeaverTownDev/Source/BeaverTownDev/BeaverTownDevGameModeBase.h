8fdf9c28e4c08e24644d347a115ac42b74489837
Magnus <magnus.sigvartsen@gmail.com>
2017-02-16 08:46:51 +0100

    Added Unreal Engine v.15 Project to repo

---
+++ BeaverTownDev/Source/BeaverTownDev/BeaverTownDevGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "BeaverTownDevGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEAVERTOWNDEV_API ABeaverTownDevGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
