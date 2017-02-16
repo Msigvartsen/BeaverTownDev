a3bc54bf9762873ef4c23830d0a22ce245879a1b
Magnus <magnus.sigvartsen@gmail.com>
2017-02-16 09:52:09 +0100

    Added character script, blueprint and HUD. Movement inputs and Dodge mechanic. Health/Stamina bar

---
+++ BeaverTownDev/Source/BeaverTownDev/MainCharacter.cpp
@@ -0,0 +1,83 @@
+
+#include "BeaverTownDev.h"
+#include "MainCharacter.h"
+
+
+AMainCharacter::AMainCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	PlayerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("PlayerCamera"));
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	PlayerCamera->SetupAttachment(CameraBoom);
+}
+
+void AMainCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	GetWorld()->GetFirstPlayerController()->bShowMouseCursor = true;
+}
+
+void AMainCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	RotateToMousePosition();
+
+	if (Stamina < 100)
+	{
+		Stamina += StaminaRegeneration * DeltaTime;
+	}
+}
+
+void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	InputComponent->BindAxis("MoveX", this, &AMainCharacter::MoveX);
+	InputComponent->BindAxis("MoveY", this, &AMainCharacter::MoveY);
+	InputComponent->BindAction("Dodge", IE_Pressed, this, &AMainCharacter::Dodge);
+}
+
+void AMainCharacter::MoveX(float value)
+{
+	AddMovementInput(FVector::ForwardVector, value);
+}
+
+void AMainCharacter::MoveY(float value)
+{
+	AddMovementInput(FVector::RightVector, value);
+}
+
+void AMainCharacter::Dodge()
+{
+	if (Stamina > 40)
+	{
+		FVector DodgeDirection(0, 0, 0);
+		DodgeDirection += FVector::ForwardVector * InputComponent->GetAxisValue("MoveX");
+		DodgeDirection += FVector::RightVector * InputComponent->GetAxisValue("MoveY");
+		DodgeDirection.Normalize();
+		LaunchCharacter((DodgeDirection*DodgeSpeed), true, false);
+		Stamina -= 40.f;
+	}
+}
+
+void AMainCharacter::RotateToMousePosition()
+{
+	FHitResult HitResult;
+	GetWorld()->GetFirstPlayerController()->GetHitResultUnderCursorByChannel(UEngineTypes::ConvertToTraceType(ECC_Camera), false, HitResult);
+	FVector PlayerToCursor = GetActorLocation() - HitResult.Location;
+	PlayerToCursor.Z = 0;
+	if (PlayerToCursor != FVector(0))
+	{
+		SetActorRotation(PlayerToCursor.Rotation());
+	}
+}
+
+float AMainCharacter::GetHealthPercent()
+{
+	return (Health / MaxHealth);
+}
+
+float AMainCharacter::GetStaminaPercent()
+{
+	return (Stamina / MaxStamina);
+}
---
+++ BeaverTownDev/Source/BeaverTownDev/MainCharacter.h
@@ -0,0 +1,50 @@
+
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "MainCharacter.generated.h"
+
+UCLASS()
+class BEAVERTOWNDEV_API AMainCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AMainCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+		UCameraComponent* PlayerCamera = nullptr;
+	UPROPERTY(EditAnywhere)
+		USpringArmComponent* CameraBoom = nullptr;
+	
+	//Input Functions
+	void MoveX(float value);
+	void MoveY(float value);
+	void Dodge();
+	void RotateToMousePosition();
+	
+	//Character Variables
+
+	UPROPERTY(EditAnywhere)
+		float DodgeSpeed = 2000.f;
+	float Stamina = 100.f;
+	float MaxStamina = 100.f;
+	float Health = 100.f;
+	float MaxHealth = 100.f;
+	float StaminaRegeneration = 3.f;
+
+	//Character Getters
+	UFUNCTION(BlueprintCallable)
+		float GetHealthPercent();
+	UFUNCTION(BlueprintCallable)
+		float GetStaminaPercent();
+
+};
