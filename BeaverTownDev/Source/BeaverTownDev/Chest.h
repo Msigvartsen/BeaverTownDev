8165a646b0885f5d7cdc36624365a95a7fbb05f7
Alexander Kamperhaug <alexanderkamperhaug@hotmail.com>
2017-02-16 16:33:02 +0100

    Added MainMenu, and interaction objects

---
+++ BeaverTownDev/Source/BeaverTownDev/Chest.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Interact.h"
+#include "Chest.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FTriggerEvent);
+
+UCLASS()
+class BEAVERTOWNDEV_API AChest : public AInteract
+{
+	GENERATED_BODY()
+
+public:
+	virtual void OpenEvent() override;
+	virtual void CloseEvent() override;
+	
+	UPROPERTY(BlueprintAssignable)
+		FTriggerEvent AlexOpen;
+	UPROPERTY(BlueprintAssignable)
+		FTriggerEvent AlexClose;
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume * Trigger = nullptr;
+
+	virtual void Tick(float DeltaTime) override;
+
+
+private:
+	bool bIsOpenEvent = false;
+
+};
---
+++ BeaverTownDev/Source/BeaverTownDev/Interact.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Interact.generated.h"
+
+UCLASS()
+class BEAVERTOWNDEV_API AInteract : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AInteract();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void OpenEvent();
+	virtual void CloseEvent();
+
+
+private:
+	UPROPERTY(EditAnywhere)
+		USceneComponent * MainRoot = nullptr;
+	UPROPERTY(EditAnywhere)
+		UStaticMeshComponent * BaseMesh = nullptr;
+
+	
+};
