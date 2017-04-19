2526d186aba85f715f556d8f30d36fb38b5b0897
Alexander Kamperhaug <alexanderkamperhaug@hotmail.com>
2017-04-19 14:03:58 +0200

    Added torches that can be picked up/dropped and ignite/extinguish other torches

--- BeaverTownDev/Source/BeaverTownDev/TorchPickup.cpp
+++ BeaverTownDev/Source/BeaverTownDev/TorchPickup.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BeaverTownDev.h"
+#include "MainCharacter.h"
 #include "TorchPickup.h"
 
 
@@ -10,13 +11,36 @@ ATorchPickup::ATorchPickup()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//// Mesh and Root
+	//TorchRoot = CreateDefaultSubobject<USceneComponent>(TEXT("Torch Root"));
+	//RootComponent = TorchRoot;
+	//TorchMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Torch Mesh"));
+	//TorchMesh->SetupAttachment(RootComponent);
+	//TorchMesh->SetSimulatePhysics(true);
+	//TorchMesh->WakeRigidBody();
+
+	//// Point Light
+	//TorchLight = CreateDefaultSubobject<UPointLightComponent>(TEXT("Torch Light"));
+	//TorchLight->SetupAttachment(TorchMesh);
+	//TorchLight->Activate();
+	//TorchLight->SetIntensity(10000.f);
+
+	//// Collisions
+	//TorchCollision = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Torch Blocking Collision"));
+	//TorchCollision->SetupAttachment(RootComponent);
+	//TorchOverlap = CreateDefaultSubobject<UBoxComponent>(TEXT("Torch Pickup Collision"));
+	//TorchOverlap->SetupAttachment(RootComponent);
+	//TorchIgniteOverlap = CreateDefaultSubobject<USphereComponent>(TEXT("Torch Ignite Collision"));
+	//TorchIgniteOverlap->SetupAttachment(RootComponent);
+
 }
 
 // Called when the game starts or when spawned
 void ATorchPickup::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	CharRef = Cast<AMainCharacter>(GetWorld()->GetFirstPlayerController()->GetCharacter());
+
 }
 
 // Called every frame
@@ -24,5 +48,17 @@ void ATorchPickup::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (IsHeld)
+	{
+		if (MeshRef)
+		{
+			MeshRef->SetSimulatePhysics(false);
+			SetActorTransform(CharRef->GetMesh()->GetSocketTransform(FName("hand_r")));
+		}
+	}
 }
 
+void ATorchPickup::Initialize(UStaticMeshComponent* MeshToSet)
+{
+	MeshRef = MeshToSet;
+}
